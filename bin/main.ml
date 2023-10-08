91e068d6fc9612d92409d613ff7ad58f48604789
Jay Lee <jaeho.lee@snu.ac.kr>
2023-10-08 18:32:04 +0900

    :sparkles: Create a dispatching driver

--- bin/main.ml
+++ bin/main.ml
@@ -1,18 +1,28 @@
+open Base
 open Stdio
-open Algorithms.Max_subarray
 
-let array =
-  [| 13; -3; -25; 20; -3; -16; -23; 18; 20; -7; 12; -5; -22; 15; -4; 7 |]
+type algorithm =
+  | Max_subarray of [ `Brute | `Divide_conquer | `Linear_rec | `Linear ]
+
+let run algorithm =
+  let open Algorithms in
+  match algorithm with
+  | Max_subarray impl ->
+      let f =
+        let open Max_subarray in
+        match impl with
+        | `Brute -> max_subarray_brute
+        | `Divide_conquer -> max_subarray_divide_conquer
+        | `Linear_rec -> max_subarray_linear_rec
+        | `Linear -> max_subarray_linear
+      in
+      let array =
+        [| 13; -3; -25; 20; -3; -16; -23; 18; 20; -7; 12; -5; -22; 15; -4; 7 |]
+      in
+      let i, j, s = f array in
+      printf "i: %d, j: %d, s: %d\n" i j s
 
 let () =
-  let i, j, s = max_subarray_brute array in
-  printf "i: %d, j: %d, s: %d\n" i j s;
-
-  let i, j, s = max_subarray_divide_conquer array in
-  printf "i: %d, j: %d, s: %d\n" i j s;
-
-  let i, j, s = max_subarray_linear_rec array in
-  printf "i: %d, j: %d, s: %d\n" i j s;
-
-  let i, j, s = max_subarray_linear array in
-  printf "i: %d, j: %d, s: %d\n" i j s
+  [ `Brute; `Divide_conquer; `Linear_rec; `Linear ]
+  |> List.map ~f:(fun x -> Max_subarray x)
+  |> List.iter ~f:run
